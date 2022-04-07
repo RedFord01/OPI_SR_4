7740a69134042e2fd44331358613d2e8ecf0bc42
Redford <103057696+RedFord01@users.noreply.github.com>
2022-04-07 13:48:37 +0000

    Додаю різницю

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() 
 {
-  int S=0, P=1, i;
+  int S=0, P=1, D=0, i;
   const int N=16;
   int mass[N];
   cout<<"Input 16 natural numbers:\n ";
@@ -14,7 +14,9 @@ int main()
     if(mass[i]%2 != 0 && mass[i]!=0) S+=mass[i];
     if(mass[i]%2 ==0) P*=mass[i];
   }
+  D = P - S;
   cout<<"Sum= "<<S<<endl;
   cout<<"Product= "<<P<<endl;
+  cout<<"Difference= "<<P<<endl;
   return 0;
 } 
