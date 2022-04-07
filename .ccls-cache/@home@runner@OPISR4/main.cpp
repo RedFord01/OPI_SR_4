c37bc9c026c90fd5605b5aad59eb31e82dcbc6de
Redford <103057696+RedFord01@users.noreply.github.com>
2022-04-07 14:10:46 +0000

    Додаю різницю

--- .ccls-cache/@home@runner@OPISR4/main.cpp
+++ .ccls-cache/@home@runner@OPISR4/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main() 
 {
-  int S=0, P=1, D=0, i;
+  int S=0, D=0, i;
   const int N=16;
   int mass[N];
   cout<<"Input 16 natural numbers:\n ";
@@ -12,11 +12,9 @@ int main()
   for(i=0;i<N;i++)
   {
     if(mass[i]%2 != 0 && mass[i]!=0) S+=mass[i];
-    if(mass[i]%2 ==0) P*=mass[i];
+    if(mass[i]/2 != 0) D-=mass[i];
   }
-  D = P - S;
   cout<<"Sum= "<<S<<endl;
-  cout<<"Product= "<<P<<endl;
   cout<<"Difference= "<<D<<endl;
   return 0;
 } 
